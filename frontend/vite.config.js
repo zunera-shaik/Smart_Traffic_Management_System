4921bf9e2c6b5d247425e3fba96fd6a872d2bcb7
SHAIK ZUNERA <shaikzunara@gmail.com>
2026-08-07 21:50:52 +0530

    Update API proxy URL in Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api': 'http://localhost:5000',
+      '/api': 'https://smart-traffic-management-system-5r2w.onrender.com/',
     },
   },
 })
